5a107802df7c44c5f45e23d6032c52f051f084cb
zourte2486 <zourte2486@gmail.com>
2025-05-16 01:17:34 +0100

    Enhance Login component with context integration and backdrop; update App to conditionally render Login based on user state

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,28 @@ import { Route, Routes, useLocation } from 'react-router';
 import Home from './pages/Home';
 import { Toaster } from 'react-hot-toast';
 import Footer from './components/Footer';
+import { useAppContext } from './context/AppContext';
+import Login from './components/Login';
 
 const App = () => {
 
     const isSellerPath = useLocation().pathname.includes("seller")
+    const {showUserLogin} = useAppContext()
 
 
   return (
     <div>
 
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
+
 
       <Toaster />
 
       <div className={`${isSellerPath ? "" : ""} px-6 md:px-16 lg:px-24 xl:px-32`}>
         <Routes>
           <Route path='/' element={<Home/>}/>
-          
+
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
