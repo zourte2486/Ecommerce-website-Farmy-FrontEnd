a9b2375aed5291743c057f456f6a39de333bc4ef
zourte2486 <zourte2486@gmail.com>
2025-05-21 19:21:56 +0100

    feat: update favicon and title, replace logo with PNG format
    Changed the favicon to 'farmy_favicon.png' and updated the page title to 'Farmy groceries' in index.html. Replaced the logo from SVG to PNG format in assets.js and removed the old SVG logo file. Deleted the unused logoFarmy.webp file to clean up assets.faicon

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import search_icon from "./search_icon.svg";
 import remove_icon from "./remove_icon.svg";
 import arrow_right_icon_colored from "./arrow_right_icon_colored.svg";
